aa3c5755c863154b84a2906facf8926cfc98d122
Alexandru Alexa <x.alx@yahoo.com>
2025-08-27 10:42:52 +0300

    Initial commit

---
+++ projects.js
@@ -0,0 +1,153 @@
+const projects = [
+    {
+        title: "E-commerce Platform",
+        description: `
+            As a visitor you can view all categories of products and if you will make a account
+            you cand add one ore more products in cart and you can make a order and
+            you can chose what option do you want for pay like PayPal or COD ( Cash On Delivery )
+            and you cand follow your oders, reaturn an order or you cand add an review to a product and more.
+            Please wait few seconds until app will loaded.
+        `,
+        technologies: "Java, Spring Boot, Spring M V C, MySQL, Junit, JS",
+        mainImage: "assets/images/projects/e-commerce/home_front.png",
+        thumbnails: [
+            "assets/images/projects/e-commerce/home_front.png",
+            "assets/images/projects/e-commerce/categories.png",
+            "assets/images/projects/e-commerce/product.png",
+            "assets/images/projects/e-commerce/login.png",
+            "assets/images/projects/e-commerce/sign_up.png",
+            "assets/images/projects/e-commerce/cart.png",
+            "assets/images/projects/e-commerce/checkout.png",
+            "assets/images/projects/e-commerce/order_complete.png",
+            "assets/images/projects/e-commerce/my_orders.png"
+        ],
+        link: "https://github.com/AlexandruAlexa1/Ecommerce"
+    },
+    {
+        title: "E-commerce Admin",
+        description: `
+            As a visitor you can view all categories of products and if you will make a account
+            you cand add one ore more products in cart and you can make a order and
+            you can chose what option do you want for pay like PayPal or COD ( Cash On Delivery )
+            and you cand follow your oders, reaturn an order or you cand add an review to a product and more.
+            Please wait few seconds until app will loaded.
+        `,
+        technologies: "Java, Spring Boot, Spring M V C, MySQL, Junit, JS",
+        mainImage: "assets/images/projects/e-commerce-admin/home.png",
+        thumbnails: [
+            "assets/images/projects/e-commerce-admin/home.png",
+            "assets/images/projects/e-commerce-admin/users.png",
+            "assets/images/projects/e-commerce-admin/categories.png",
+            "assets/images/projects/e-commerce-admin/brands.png",
+            "assets/images/projects/e-commerce-admin/products.png",
+            "assets/images/projects/e-commerce-admin/reviews.png",
+            "assets/images/projects/e-commerce-admin/questions.png",
+            "assets/images/projects/e-commerce-admin/customers.png",
+            "assets/images/projects/e-commerce-admin/shipping.png",
+            "assets/images/projects/e-commerce-admin/orders.png",
+            "assets/images/projects/e-commerce-admin/sales_report.png",
+            "assets/images/projects/e-commerce-admin/articles.png",
+            "assets/images/projects/e-commerce-admin/menu.png",
+            "assets/images/projects/e-commerce-admin/home_page.png",
+            "assets/images/projects/e-commerce-admin/settings.png",
+            "assets/images/projects/e-commerce-admin/admin.png"
+        ],
+        link: "https://github.com/AlexandruAlexa1/Ecommerce"
+    },
+
+     {
+        title: "spring-cloud-ecommerce",
+        description: "A Spring Cloud microservices project with service discovery, centralized config, async messaging, distributed tracing, monitoring, and secure secrets management. Fully Dockerized for easy local development.",
+        technologies: "Java, Spring Boot, Spring Cloud, Docker",
+        mainImage: "assets/images/projects/spring-cloud-project/Eureka.png",
+        thumbnails: [
+            "assets/images/projects/spring-cloud-project/DockerCompose.png",
+            "assets/images/projects/spring-cloud-project/Products.png",
+            "assets/images/projects/spring-cloud-project/Eureka.png",
+            "assets/images/projects/spring-cloud-project/Zipkin.png",
+            "assets/images/projects/spring-cloud-project/RabbitMQ.png",
+            "assets/images/projects/spring-cloud-project/CreateOrder.png",
+            "assets/images/projects/spring-cloud-project/ListNotifications.png",
+        ],
+        link: "https://github.com/AlexandruAlexa1/spring-cloud-ecommerce"
+    },
+
+     {
+        title: "spring-cloud-k8s-ecommerce",
+        description: "E-commerce microservices platform with Spring Cloud & Kubernetes",
+        technologies: "Java, Spring Boot, Spring Cloud, Kubernetes",
+        mainImage: "assets/images/projects/spring-cloud-k8s/prometheus.png",
+        thumbnails: [
+            "assets/images/projects/spring-cloud-k8s/prometheus.png",
+            "assets/images/projects/spring-cloud-k8s/gateway.png",
+            "assets/images/projects/spring-cloud-k8s/grafana.png",
+            "assets/images/projects/spring-cloud-k8s/zipkin.png",
+            "assets/images/projects/spring-cloud-k8s/zipkin2.png"
+        ],
+        link: "https://github.com/AlexandruAlexa1/spring-cloud-k8s-ecommerce"
+    },
+
+    {
+        title: "Car Rental Full Stack Project",
+        description: "Descriere scurtă a Proiectului 2",
+        technologies: "Java, Spring Boot, MySQL, Angular",
+        mainImage: "assets/images/projects/car-rental/a1.png",
+        thumbnails: [
+            "assets/images/projects/car-rental/a1.png",
+            "assets/images/projects/car-rental/a2.png",
+            "assets/images/projects/car-rental/a3.png",
+            "assets/images/projects/car-rental/a4.png",
+            "assets/images/projects/car-rental/a5.png",
+            "assets/images/projects/car-rental/a6.png",
+        ],
+        link: "https://github.com/AlexandruAlexa1/car-rental"
+    },
+    {
+        title: "Architecture Project",
+        description: "Descriere scurtă a Proiectului 3",
+        technologies: "HTML, CSS, JS",
+        mainImage: "assets/images/projects/ArchitectureProject.png",
+        thumbnails: [
+           
+        ],
+        link: "https://alexandrualexa1.github.io/ArchitectureProject"
+    },
+    {
+        title: "Time Project",
+        description: "Descriere scurtă a Proiectului 4",
+        technologies: "HTML, CSS, JS",
+        mainImage: "assets/images/projects/TimeProject.png",
+        thumbnails: [
+          
+        ],
+        link: "https://alexandrualexa1.github.io/TimeProject"
+    },
+    {
+        title: "Spring Boot REST API Secured with Spring Security & JWT",
+        description: "User Management, Exception Handling, Validation, E-mail Integration, Encoded Passwords, User Registration, Roles Authorization, Login, Unit Testing, Integration Testing",
+        technologies: "Java, Spring Boot, Spring Security, JWT",
+        mainImage: "assets/images/projects/spring-boot-angular-jwt-img/b1.png",
+        thumbnails: [
+           "assets/images/projects/spring-boot-angular-jwt-img/b1.png",
+           "assets/images/projects/spring-boot-angular-jwt-img/b2.png",
+           "assets/images/projects/spring-boot-angular-jwt-img/b3.png",
+           "assets/images/projects/spring-boot-angular-jwt-img/b4.png",
+           "assets/images/projects/spring-boot-angular-jwt-img/b5.png",
+           "assets/images/projects/spring-boot-angular-jwt-img/b6.png"
+        ],
+        link: "https://github.com/AlexandruAlexa1/spring-boot-angular-jwt"
+    },
+    {
+        title: "User Management",
+        description: "List users, add new user, edit user, delete user, view details, filtering, sorting and paginations.",
+        technologies: "Java, Spring Boot, Spring MVC with Thymeleaf, H2 DB",
+        mainImage: "assets/images/projects/user-management/s1.png",
+        thumbnails: [
+           "assets/images/projects/user-management/s1.png",
+           "assets/images/projects/user-management/s2.png",
+           "assets/images/projects/user-management/s3.png",
+           "assets/images/projects/user-management/s4.png",
+        ],
+        link: "https://github.com/AlexandruAlexa1/UserManagement"
+    }
+];
---
+++ script.js
@@ -0,0 +1,142 @@
+let currentSlide = 0;
+let autoSlideInterval;
+const slider = document.getElementById("slider");
+const cardContainer = document.getElementById("card-container");
+const cards = [];
+const slides = [];
+
+function loadProjects() {
+    projects.forEach((project, index) => {
+        const slide = document.createElement("div");
+        slide.classList.add("slide");
+
+        const thumbnailsHtml = project.thumbnails.map((thumbnail, thumbIndex) =>
+            `<img src="${thumbnail}" alt="Thumbnail" class="thumbnail" data-project-index="${index}" data-thumb-index="${thumbIndex}">`
+        ).join('');
+
+        slide.innerHTML = `
+            <img class="main-image" src="${project.mainImage}" alt="${project.title}">
+            <div class="project-details">
+                <h3>${project.title}</h3>
+               
+                <p><strong>Technologies:</strong> ${project.technologies}</p>
+                <a href="${project.link}" target="blank">View details</a>
+            </div>
+            <div class="project-thumbnails">
+                ${thumbnailsHtml}
+            </div>
+        `;
+
+        slides.push(slide);
+        slider.appendChild(slide);
+
+        if (index < 5) {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `<img src="${project.mainImage}" alt="${project.title}">`;
+            cardContainer.appendChild(card);
+            cards.push(card);
+
+            card.addEventListener("click", () => {
+                currentSlide = index;
+                updateSliderPosition();
+                updateCardSelection();
+            });
+        }
+
+        const largeCardContainer = document.querySelector('.responsive-projects');
+        const largeCard = createLargeCard(project);
+        largeCardContainer.appendChild(largeCard);
+    });
+
+    updateCardSelection();
+    addThumbnailEventListeners();
+}
+
+function createLargeCard(project) {
+    const largeCard = document.createElement('div');
+    largeCard.classList.add('large-card');
+
+    largeCard.innerHTML = `
+        <ul class="projects-list">
+            <li>
+                <h3>${project.title}</h3>
+                <a href="">View details</a>
+            </li>
+        </ul>
+    `
+
+    return largeCard;
+}
+
+
+// Adaugă ascultători de evenimente pentru thumbnail-uri
+function addThumbnailEventListeners() {
+    document.querySelectorAll('.thumbnail').forEach(thumbnail => {
+        thumbnail.addEventListener('click', (event) => {
+            const projectIndex = parseInt(event.target.getAttribute('data-project-index'));
+            const thumbIndex = parseInt(event.target.getAttribute('data-thumb-index'));
+            const project = projects[projectIndex];
+            const newMainImage = project.thumbnails[thumbIndex];
+            slides[projectIndex].querySelector('.main-image').src = newMainImage;
+        });
+    });
+}
+
+// Actualizează poziția sliderului
+function updateSliderPosition() {
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+    if (currentSlide % 5 === 0) {
+        updateCardList();
+    }
+    updateCardSelection();
+}
+
+// Actualizează lista de carduri afișate
+function updateCardList() {
+    const startIndex = Math.floor(currentSlide / 5) * 5;
+    cardContainer.querySelectorAll(".card").forEach((card, index) => {
+        const projectIndex = startIndex + index;
+        if (projects[projectIndex]) {
+            card.querySelector("img").src = projects[projectIndex].mainImage;
+        }
+    });
+}
+
+// Evidențiază cardul corespunzător sliderului curent
+function updateCardSelection() {
+    cards.forEach((card, index) => {
+        card.classList.toggle("active", index === (currentSlide % 5));
+    });
+}
+
+// Schimbă la slide-ul următor
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % projects.length;
+    updateSliderPosition();
+}
+
+// Schimbă la slide-ul anterior
+function previousSlide() {
+    currentSlide = (currentSlide - 1 + projects.length) % projects.length;
+    updateSliderPosition();
+}
+
+// Începe schimbarea automată a slide-urilor
+function startAutoSlide() {
+    autoSlideInterval = setInterval(nextSlide, 5000); // Schimbă slide-ul la fiecare 5 secunde
+}
+
+// Oprește schimbarea automată a slide-urilor
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadProjects();
+    // startAutoSlide();
+
+    const sliderContainer = document.querySelector(".slider-container");
+    sliderContainer.addEventListener("mouseover", stopAutoSlide);
+    sliderContainer.addEventListener("mouseout", startAutoSlide);
+});
